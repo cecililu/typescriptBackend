04fa292a64b8223bbe32a746501cab4f0003e0ee
cecil <cecilghimire@gmail.com>
2022-09-20 20:20:33 +0545

    added protected route handler

--- src/routes/auth/profile.ts
+++ src/routes/auth/profile.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
+import { getProfileHandler } from "../../controllers/profile";
 
 
 const router=Router()
 
 
-router.get('/Profilelocked',
-
-)
+router.get('/Profilelocked',getPrfileHandler)
 
 export{router as getProfileRouter}
